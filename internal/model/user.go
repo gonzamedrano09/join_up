3574d6d721862a53ff625089fdc80ee5debade93
Gonzalo Medrano <gonzalomedrano09@gmail.com>
2023-08-06 19:38:13 -0300

    Update user model

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,12 +1,15 @@
 package model
 
 type User struct {
+	Username       string
 	Email          string
 	HashedPassword string
-	FirstName      string
-	LastName       string
-	Biography      string
-	Tags           []Tag
-	// Picture
-	// Location
+
+	Picture   string
+	FirstName string
+	LastName  string
+	Biography string
+	Location  Location
+
+	Tags []Tag
 }
