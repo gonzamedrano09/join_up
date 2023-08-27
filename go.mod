4c2c508b03fc9a64e15f3a50595c0ade4ad29e0a
Gonzalo Medrano <gonzalomedrano09@gmail.com>
2023-08-27 17:25:40 -0300

    Develop user model with inputs and some password methods

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module main
 
 go 1.20
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	golang.org/x/crypto v0.12.0
+)
--- internal/model/user.go
+++ internal/model/user.go
@@ -1,5 +1,54 @@
 package model
 
+import (
+	"encoding/json"
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+// Inputs
+
+type UserCreateInput struct {
+	Username string
+	Password string
+
+	FirstName string
+	LastName  string
+}
+
+func (u *UserCreateInput) IsValid() bool {
+	return true
+}
+
+type UserUpdateInput struct {
+	FirstName string
+	LastName  string
+	Biography string
+}
+
+func (u *UserUpdateInput) IsValid() bool {
+	return true
+}
+
+type UserUpdateLocationsInput struct {
+	Locations []Location
+}
+
+func (u *UserUpdateLocationsInput) IsValid() bool {
+	return true
+}
+
+type UserUpdateTagsInput struct {
+	Tags []Tag
+}
+
+func (u *UserUpdateTagsInput) IsValid() bool {
+	return true
+}
+
+// Model
+
 type User struct {
 	Username       string
 	Email          string
@@ -9,7 +58,41 @@ type User struct {
 	FirstName string
 	LastName  string
 	Biography string
-	Location  Location
 
-	Tags []Tag
+	Locations []Location
+	Tags      []Tag
+}
+
+func NewUser(ui *UserCreateInput) (*User, error) {
+	userBytes, err := json.Marshal(ui)
+	if err != nil {
+		return nil, errors.New("error creating user")
+	}
+
+	user := User{}
+	if err = json.Unmarshal(userBytes, &user); err != nil {
+		return nil, errors.New("error creating user")
+	}
+
+	if err = user.SetPassword(ui.Password); err != nil {
+		return nil, errors.New("error creating user")
+	}
+
+	return &user, nil
+}
+
+func (u *User) SetPassword(password string) error {
+	passwordBytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		return errors.New("error setting password user")
+	}
+
+	u.HashedPassword = string(passwordBytes)
+
+	return nil
+}
+
+func (u *User) CheckPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.HashedPassword), []byte(password))
+	return err == nil
 }
