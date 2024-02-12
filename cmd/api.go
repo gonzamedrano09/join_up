9de2eb02db4c649dbbbccde064e9c43b62a35bec
Gonza Medrano <gonzalomedrano09@gmail.com>
2024-02-12 17:39:15 -0300

    Inject new dependencies in api struct, add error return

--- cmd/api.go
+++ cmd/api.go
@@ -1,17 +1,80 @@
 package main
 
 import (
-	"main/internal/controller"
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/gonzamedrano09/join_up/internal/config"
+	"github.com/gonzamedrano09/join_up/internal/controller"
+	"github.com/gonzamedrano09/join_up/internal/repository"
+	"github.com/gonzamedrano09/join_up/internal/service"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	usersCollection = "users"
 )
 
 type API struct {
-	UserController controller.UserController
+	clients struct {
+		dbConnection *mongo.Client
+	}
+
+	repositories struct {
+		userRepository repository.UserRepository
+	}
+
+	services struct {
+		userService service.UserService
+	}
+
+	controllers struct {
+		userController controller.UserController
+	}
 }
 
-func NewAPI() *API {
-	// Define controllers
-	userController := controller.NewUserController()
+func NewAPI() (*API, error) {
+	// Init API
+	api := new(API)
+
+	// Create clients
+	dbConnection, err := mongo.Connect(
+		context.Background(),
+		options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%s@%s:%s/",
+			config.Config.DatabaseUser,
+			config.Config.DatabasePassword,
+			config.Config.DatabaseHost,
+			config.Config.DatabasePort,
+		)),
+	)
+	if err != nil {
+		return nil, errors.New("error creating dbConnection")
+	}
+	api.clients.dbConnection = dbConnection
+
+	// Create repositories
+	userRepository, err := repository.NewUserRepository(dbConnection, config.Config.DatabaseName, usersCollection)
+	if err != nil {
+		return nil, errors.New("error creating userRepository")
+	}
+	api.repositories.userRepository = userRepository
+
+	// Create services
+	userService, err := service.NewUserService(userRepository)
+	if err != nil {
+		return nil, errors.New("error creating userService")
+	}
+	api.services.userService = userService
+
+	// Create controllers
+	userController, err := controller.NewUserController(userService)
+	if err != nil {
+		return nil, errors.New("error creating userController")
+	}
+	api.controllers.userController = userController
 
 	// Return pointer to API struct
-	return &API{UserController: userController}
+	return api, nil
 }
