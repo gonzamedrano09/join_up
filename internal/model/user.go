cc69c57e16a557696887a812f68edb44bcb61bfb
Gonza Medrano <gonzalomedrano09@gmail.com>
2024-02-12 17:37:58 -0300

    Change return type in isValid functions in user input models

--- internal/model/user.go
+++ internal/model/user.go
@@ -3,6 +3,7 @@ package model
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 
 	"golang.org/x/crypto/bcrypt"
 )
@@ -17,8 +18,8 @@ type UserCreateInput struct {
 	LastName  string
 }
 
-func (u *UserCreateInput) IsValid() bool {
-	return true
+func (u *UserCreateInput) IsValid() error {
+	return nil
 }
 
 type UserUpdateInput struct {
@@ -27,24 +28,24 @@ type UserUpdateInput struct {
 	Biography string
 }
 
-func (u *UserUpdateInput) IsValid() bool {
-	return true
+func (u *UserUpdateInput) IsValid() error {
+	return nil
 }
 
 type UserUpdateLocationsInput struct {
 	Locations []Location
 }
 
-func (u *UserUpdateLocationsInput) IsValid() bool {
-	return true
+func (u *UserUpdateLocationsInput) IsValid() error {
+	return nil
 }
 
 type UserUpdateTagsInput struct {
 	Tags []Tag
 }
 
-func (u *UserUpdateTagsInput) IsValid() bool {
-	return true
+func (u *UserUpdateTagsInput) IsValid() error {
+	return nil
 }
 
 // Model
@@ -66,16 +67,16 @@ type User struct {
 func NewUser(ui *UserCreateInput) (*User, error) {
 	userBytes, err := json.Marshal(ui)
 	if err != nil {
-		return nil, errors.New("error creating user")
+		return nil, errors.New(fmt.Sprintf("error creating user: %s", err))
 	}
 
 	user := User{}
 	if err = json.Unmarshal(userBytes, &user); err != nil {
-		return nil, errors.New("error creating user")
+		return nil, errors.New(fmt.Sprintf("error creating user: %s", err))
 	}
 
 	if err = user.SetPassword(ui.Password); err != nil {
-		return nil, errors.New("error creating user")
+		return nil, errors.New(fmt.Sprintf("error creating user: %s", err))
 	}
 
 	return &user, nil
@@ -84,7 +85,7 @@ func NewUser(ui *UserCreateInput) (*User, error) {
 func (u *User) SetPassword(password string) error {
 	passwordBytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	if err != nil {
-		return errors.New("error setting password user")
+		return errors.New(fmt.Sprintf("error setting password user: %s", err))
 	}
 
 	u.HashedPassword = string(passwordBytes)
